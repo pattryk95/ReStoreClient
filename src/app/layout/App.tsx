d906d35671b372560ff2b9beda3e4977c5335544
Patryk <patryk.bogdanski95@gmail.com>
2022-01-17 20:37:43 +0100

    Add react-toastify to display custom error notifications

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -6,12 +6,14 @@ import {
 } from "@mui/material";
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
 import ProductDetails from "../../features/catalog/ProductDetails";
 import ContactPage from "../../features/contact/ContantPage";
 import HomePage from "../../features/home/HomePage";
 import Header from "./Header";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -32,6 +34,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position='bottom-right' hideProgressBar theme="colored" />
       <CssBaseline />
       <Header handleChange={handleChange} darkMode={darkMode} />
       <Container>
